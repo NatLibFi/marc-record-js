c916dac58b01f268608fc659b7e59fef34088d64
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-08-06 11:24:57 +0300

    Refactor with new ESLint

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -3,7 +3,7 @@
 * @licstart  The following is the entire license notice for the JavaScript code in this file.
 *
 * Copyright 2014-2017 Pasi Tuominen
-* Copyright 2018 University Of Helsinki (The National Library Of Finland)
+* Copyright 2018-2020 University Of Helsinki (The National Library Of Finland)
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 *
@@ -16,70 +16,67 @@
 *
 */
 
-/* eslint-disable no-unused-expressions */
 import {expect} from 'chai';
 import * as Utils from './utils';
 
 describe('utils', () => {
-	describe('#clone', () => {
-		it('Should clone an object', () => {
-			const a = {foo: 'bar'};
-			const b = Utils.clone(a);
+  describe('#clone', () => {
+    it('Should clone an object', () => {
+      const a = {foo: 'bar'};
+      const b = Utils.clone(a);
 
-			expect(JSON.stringify(a)).to.equal(JSON.stringify(b));
-			expect(a).to.be.eql(b);
-			expect(a).to.not.equal(b);
-		});
-	});
+      expect(JSON.stringify(a)).to.equal(JSON.stringify(b));
+      expect(a).to.be.eql(b);
+      expect(a).to.not.equal(b);
+    });
+  });
 
-	describe('#validateRecord', () => {
-		it('Should consider the record valid', () => {
-			const record = {
-				leader: 'foo',
-				fields: [
-					{tag: 'FOO', value: 'bar'}
-				]
-			};
+  describe('#validateRecord', () => {
+    it('Should consider the record valid', () => {
+      const record = {
+        leader: 'foo',
+        fields: [{tag: 'FOO', value: 'bar'}]
+      };
 
-			expect(Utils.validateRecord(record)).to.not.throw;
-		});
+      expect(Utils.validateRecord(record)).to.not.throw; // eslint-disable-line no-unused-expressions
+    });
 
-		it('Should consider the record invalid', () => {
-			const record = {
-				leader: 'foo'
-			};
+    it('Should consider the record invalid', () => {
+      const record = {
+        leader: 'foo'
+      };
 
-			try {
-				Utils.validateRecord(record);
-			} catch (err) {
-				expect(err.message).to.match(/^Record is invalid$/);
-				expect(err).to.have.property('validationResults');
-				return;
-			}
+      try {
+        Utils.validateRecord(record);
+      } catch (err) {
+        expect(err.message).to.match(/^Record is invalid$/u);
+        expect(err).to.have.property('validationResults');
+        return;
+      }
 
-			throw new Error('Should throw');
-		});
-	});
+      throw new Error('Should throw');
+    });
+  });
 
-	describe('#validateField', () => {
-		it('Should consider the field valid', () => {
-			const field = {tag: 'FOO', value: 'BAR'};
+  describe('#validateField', () => {
+    it('Should consider the field valid', () => {
+      const field = {tag: 'FOO', value: 'BAR'};
 
-			expect(Utils.validateField(field)).to.not.throw;
-		});
+      expect(Utils.validateField(field)).to.not.throw; // eslint-disable-line no-unused-expressions
+    });
 
-		it('Should consider the field invalid', () => {
-			const field = {tag: 'FOO'};
+    it('Should consider the field invalid', () => {
+      const field = {tag: 'FOO'};
 
-			try {
-				Utils.validateField(field);
-			} catch (err) {
-				expect(err.message).to.match(/^Field is invalid: {"tag":"FOO"}$/);
-				expect(err).to.have.property('validationResults');
-				return;
-			}
+      try {
+        Utils.validateField(field);
+      } catch (err) {
+        expect(err.message).to.match(/^Field is invalid: \{"tag":"FOO"\}$/u);
+        expect(err).to.have.property('validationResults');
+        return;
+      }
 
-			throw new Error('Should throw');
-		});
-	});
+      throw new Error('Should throw');
+    });
+  });
 });
