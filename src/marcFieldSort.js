8331bdc1fed0296919c1fb8b449365599208c109
nvolk <nicholas.volk@helsinki.fi>
2023-10-10 21:34:12 +0300

    Remove Melinda specific sorting rules. Keep only the generic ones.

--- src/marcFieldSort.js
+++ src/marcFieldSort.js
@@ -6,9 +6,9 @@ const debugDev = debug.extend('dev');
 
 
 export function fieldOrderComparator(fieldA, fieldB) {
-  const BIG_BAD_NUMBER = 999.99;
   //const sorterFunctions = [sortByTag, sortByLOW, sortBySID, sortByIndexTerms, sortAlphabetically];
-  const sorterFunctions = [sortByTag, /*sortByLOW,*/ /*sortBySID,*/ sortByIndexTerms, sortAlphabetically, preferFenniKeep];
+  //const sorterFunctions = [sortByTag, /*sortByLOW,*/ /*sortBySID,*/ sortByIndexTerms, sortAlphabetically, preferFenniKeep];
+  const sorterFunctions = [sortByTag, sortAlphabetically];
 
   function fieldToString(f) {
     if ('subfields' in f) {
@@ -96,135 +96,9 @@ export function fieldOrderComparator(fieldA, fieldB) {
   }
 */
 
-  function sortByIndexTerms(fieldA, fieldB) { // eslint-disable-line complexity, max-statements
-
-    const indexTermFields = [
-      '600',
-      '610',
-      '611',
-      '630',
-      '648',
-      '650',
-      '651',
-      '652',
-      '653',
-      '654',
-      '655',
-      '656',
-      '657',
-      '658',
-      '659',
-      '662'
-    ];
-
-    function scoreInd2(val) {
-      const ind2Score = {
-        '0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 8,
-        '5': 5,
-        '6': 6,
-        '7': 7
-      };
-
-      if (val in ind2Score) {
-        return ind2Score[val];
-      }
-      return 9;
-    }
-
-    // ATM this is not needed.
-    // You may need this, if you change compare function order in sorterFunctions
-    // istanbul ignore next
-    if (fieldA.tag !== fieldB.tag) {
-      return 0;
-    }
-
-    if (!indexTermFields.includes(fieldA.tag)) {
-      return 0;
-    }
-
-    /* Puts ind2=4 last */
-    if (scoreInd2(fieldA.ind2) > scoreInd2(fieldB.ind2)) {
-      return 1;
-    }
-    if (scoreInd2(fieldA.ind2) < scoreInd2(fieldB.ind2)) {
-      return -1;
-    }
-
-    function scoreDictionary(dictionary) {
-      const dictionarySortIndex = {
-        'yso/fin': 0,
-        'yso/swe': 1,
-        'yso/eng': 2,
-        'slm/fin': 0.1,
-        'slm/swe': 1.1,
-        'kaunokki': 4,
-        'bella': 5
-      };
-
-      if (dictionary in dictionarySortIndex) {
-        return dictionarySortIndex[dictionary];
-      }
-      return BIG_BAD_NUMBER;
-    }
-
-    const dictionaryA = selectFirstValue(fieldA, '2');
-    const dictionaryB = selectFirstValue(fieldB, '2');
-
-    const dictScoreA = scoreDictionary(dictionaryA);
-    const dictScoreB = scoreDictionary(dictionaryB);
-    // Use priority order for listed dictionaries:
-    if (dictScoreA > dictScoreB) {
-      return 1;
-    }
-    if (dictScoreA < dictScoreB) {
-      return -1;
-    }
-    // Unlisted dictionaries: sort $2 value alphabetically:
-    //if (dictScoreA === BIG_BAD_NUMBER) {
-    if (dictionaryA > dictionaryB) {
-      return 1;
-    }
-    if (dictionaryA < dictionaryB) {
-      return -1;
-    }
-    //}
-    return 0;
-  }
-
-  function preferKeep(fieldA, fieldB, keepOwner = 'FENNI') {
-    const keepSelector = fieldHasSubfield('9', `${keepOwner}<KEEP>`);
-    const hasKeepA = keepSelector(fieldA);
-    const hasKeepB = keepSelector(fieldB);
-
-    if (hasKeepA && !hasKeepB) {
-      return -1;
-    }
-    if (!hasKeepA && hasKeepB) {
-      return 1;
-    }
-
-    return 0;
-  }
-
-  function preferFenniKeep(fieldA, fieldB) {
-    const fenniPreference = preferKeep(fieldA, fieldB, 'FENNI');
-    if (fenniPreference !== 0) {
-      return fenniPreference;
-    }
-    const violaPreference = preferKeep(fieldA, fieldB, 'VIOLA');
-    if (violaPreference !== 0) {
-      return violaPreference;
-    }
-    return preferKeep(fieldA, fieldB, 'FIKKA');
-  }
-
   function sortAlphabetically(fieldA, fieldB) {
+    // These are Aleph specific rules. However, we think that they are ok for all Aleph users, and irrelevant for others.
     const tagToSortingSubfields = {
-      // '028': ['b', 'a']?
       'LOW': ['a'],
       'SID': ['c']
     };
@@ -270,14 +144,6 @@ export function fieldOrderComparator(fieldA, fieldB) {
       return result;
     }
     return 0;
-  }
-
-  //-----------------------------------------------------------------------------
-
-
-  function fieldHasSubfield(subcode, value) {
-    return (field) => field.subfields && field.subfields
-      .some(subfield => subcode === subfield.code && subfield.value === value);
   }
 
   function selectFirstValue(field, subcode) {
