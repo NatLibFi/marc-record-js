f3adb104be1377a388195eb9b15afe4e8a6b6b21
nvolk <nicholas.volk@helsinki.fi>
2023-10-10 21:43:15 +0300

    Add new paramater 'sortFunctions' to fieldOrderComparator. Export sortByTag and sortAlphabetically export, so that they can be used by a validator.

--- src/marcFieldSort.js
+++ src/marcFieldSort.js
@@ -4,11 +4,9 @@ const debug = createDebugLogger('@natlibfi/marc-record:marcFieldSort');
 //const debugData = debug.extend('data');
 const debugDev = debug.extend('dev');
 
+export const defaultSorterFunctions = [sortByTag, sortAlphabetically];
 
-export function fieldOrderComparator(fieldA, fieldB) {
-  //const sorterFunctions = [sortByTag, sortByLOW, sortBySID, sortByIndexTerms, sortAlphabetically];
-  //const sorterFunctions = [sortByTag, /*sortByLOW,*/ /*sortBySID,*/ sortByIndexTerms, sortAlphabetically, preferFenniKeep];
-  const sorterFunctions = [sortByTag, sortAlphabetically];
+export function fieldOrderComparator(fieldA, fieldB, sorterFunctions = defaultSorterFunctions) {
 
   function fieldToString(f) {
     if ('subfields' in f) {
@@ -29,127 +27,96 @@ export function fieldOrderComparator(fieldA, fieldB) {
   }
 
   return 0;
+}
 
-  function sortByTag(fieldA, fieldB) {
+export function sortByTag(fieldA, fieldB) {
 
-    function getSortIndex(tag) {
-      const sortIndex = {
-        LDR: '000',
-        STA: '001.1', // STA comes now after 001. However 003+001 form a combo, so I'm not sure...
-        SID: '999.1',
-        LOW: '999.2',
-        CAT: '999.3',
-        HLI: '999.4'
-      };
-
-      if (tag in sortIndex) { // <- this allows weights for numeric values as well (not that we use them yet)
-        return sortIndex[tag];
-      }
-      if (isNaN(tag)) {
-        return '999.9';
-      }
-      return tag;
-    }
-
-    const orderA = getSortIndex(fieldA.tag);
-    const orderB = getSortIndex(fieldB.tag);
-
-    if (orderA > orderB) {
-      return 1;
-    }
-    if (orderA < orderB) {
-      return -1;
-    }
-
-    return 0;
-  }
-
-  /*
-  function sortByLOW(fieldA, fieldB) {
-    if (fieldA.tag === 'LOW' && fieldB.tag === 'LOW') {
-      const lowA = selectFirstValue(fieldA, 'a');
-      const lowB = selectFirstValue(fieldB, 'a');
-      if (lowA > lowB) {
-        return 1;
-      }
-      if (lowA < lowB) {
-        return -1;
-      }
-    }
-    return 0;
-  }
-  */
-
-  /*
-  function sortBySID(fieldA, fieldB) {
-    if (fieldA.tag === 'SID' && fieldB.tag === 'SID') {
-      const sidA = selectFirstValue(fieldA, 'c');
-      const sidB = selectFirstValue(fieldB, 'c');
-      if (sidA > sidB) {
-        return 1;
-      }
-      if (sidA < sidB) {
-        return -1;
-      }
-    }
-    return 0;
-  }
-*/
-
-  function sortAlphabetically(fieldA, fieldB) {
-    // These are Aleph specific rules. However, we think that they are ok for all Aleph users, and irrelevant for others.
-    const tagToSortingSubfields = {
-      'LOW': ['a'],
-      'SID': ['c']
+  function getSortIndex(tag) {
+    const sortIndex = {
+      LDR: '000',
+      STA: '001.1', // STA comes now after 001. However 003+001 form a combo, so I'm not sure...
+      SID: '999.1',
+      LOW: '999.2',
+      CAT: '999.3',
+      HLI: '999.4'
     };
 
-    function scoreSubfieldsAlphabetically(setOfSubfields) {
-      if (setOfSubfields.length === 0) {
-        return 0;
-      }
-      const [subfieldCode, ...remainingSubfieldCodes] = setOfSubfields;
-      const valA = selectFirstValue(fieldA, subfieldCode);
-      const valB = selectFirstValue(fieldB, subfieldCode);
-      //debugDev(`CHECKING SUBFIELD '${subfieldCode}'`);
-      if (!valA) {
-        if (!valB) {
-          return scoreSubfieldsAlphabetically(remainingSubfieldCodes);
-        }
-        return -1;
-      }
+    if (tag in sortIndex) { // <- this allows weights for numeric values as well (not that we use them yet)
+      return sortIndex[tag];
+    }
+    if (isNaN(tag)) {
+      return '999.9';
+    }
+    return tag;
+  }
+
+  const orderA = getSortIndex(fieldA.tag);
+  const orderB = getSortIndex(fieldB.tag);
+
+  if (orderA > orderB) {
+    return 1;
+  }
+  if (orderA < orderB) {
+    return -1;
+  }
+
+  return 0;
+}
+
+
+export function sortAlphabetically(fieldA, fieldB) {
+  // These are Aleph specific rules. However, we think that they are ok for all Aleph users, and irrelevant for others.
+  const tagToSortingSubfields = {
+    'LOW': ['a'],
+    'SID': ['c']
+  };
+
+  function scoreSubfieldsAlphabetically(setOfSubfields) {
+    if (setOfSubfields.length === 0) {
+      return 0;
+    }
+    const [subfieldCode, ...remainingSubfieldCodes] = setOfSubfields;
+    const valA = selectFirstValue(fieldA, subfieldCode);
+    const valB = selectFirstValue(fieldB, subfieldCode);
+    //debugDev(`CHECKING SUBFIELD '${subfieldCode}'`);
+    if (!valA) {
       if (!valB) {
-        return 1;
+        return scoreSubfieldsAlphabetically(remainingSubfieldCodes);
       }
-      debugDev(`CHECKING SUBFIELD '${subfieldCode}': '${valA}' vs '${valB}'`);
-
-      if (valA < valB) {
-        return -1;
-      }
-      if (valB < valA) {
-        return 1;
-      }
-      return scoreSubfieldsAlphabetically(remainingSubfieldCodes);
+      return -1;
     }
-
-    if (fieldA.tag === fieldB.tag) {
-      if (!(fieldA.tag in tagToSortingSubfields)) {
-        return 0;
-      }
-
-      const subfieldsToCheck = tagToSortingSubfields[fieldA.tag];
-
-      //debugDev(`CHECKING ${subfieldsToCheck.join(', ')}`);
-      const result = scoreSubfieldsAlphabetically(subfieldsToCheck);
-      debugDev(`RESULT ${result}`);
-      return result;
+    if (!valB) {
+      return 1;
     }
-    return 0;
+    debugDev(`CHECKING SUBFIELD '${subfieldCode}': '${valA}' vs '${valB}'`);
+
+    if (valA < valB) {
+      return -1;
+    }
+    if (valB < valA) {
+      return 1;
+    }
+    return scoreSubfieldsAlphabetically(remainingSubfieldCodes);
   }
 
-  function selectFirstValue(field, subcode) {
-    return field.subfields
-      .filter(subfield => subcode === subfield.code)
-      .map(subfield => subfield.value)
-      .slice(0, 1);
+  if (fieldA.tag === fieldB.tag) {
+    if (!(fieldA.tag in tagToSortingSubfields)) {
+      return 0;
+    }
+
+    const subfieldsToCheck = tagToSortingSubfields[fieldA.tag];
+
+    //debugDev(`CHECKING ${subfieldsToCheck.join(', ')}`);
+    const result = scoreSubfieldsAlphabetically(subfieldsToCheck);
+    debugDev(`RESULT ${result}`);
+    return result;
   }
+  return 0;
+}
+
+function selectFirstValue(field, subcode) {
+  return field.subfields
+    .filter(subfield => subcode === subfield.code)
+    .map(subfield => subfield.value)
+    .slice(0, 1);
 }
